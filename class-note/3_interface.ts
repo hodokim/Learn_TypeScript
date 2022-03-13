40c6330af7a4d8b120e9b450c3c7679b250d67ab
hodokim <hodo323@naver.com>
2022-03-13 11:13:57 +0900

    Ts InterFace

---
+++ class-note/3_interface.ts
@@ -0,0 +1,47 @@
+interface Person {
+    age: number;
+    name: string;
+    address: string;
+}
+
+//변수에 인터페이스 활용
+var baha: Person = {
+    age: 33,
+    name: '바하',
+    address: '인천',
+}
+
+// 함수에 인터페이스 활용
+function getUser(user: Person) {
+    console.log(user.toString());
+}
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+sum = function(a:number, b:number): number {
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+
+let arr: StringArray = ['a','b','c'];
+
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp;
+}
+
+let obj: StringRegexDictionary = {
+    // sth: /abc/,
+    cssFile: /\.css$/,
+    jsFile: /\.js$/,
+}
+
