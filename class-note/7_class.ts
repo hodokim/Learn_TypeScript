a7a06b5d14ded6fa02b8c98fb3c1b906c76f868f
hodokim <hodo323@naver.com>
2022-03-17 23:58:32 +0900

    JS & TS Classes

---
+++ class-note/7_class.ts
@@ -0,0 +1,11 @@
+class Person3 {
+    public name: string;
+    private age: number;
+    readonly address: string;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,15 @@
+//ES6 이전에서의 Class 대용 작성 방법
+function Person2(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let alvarez = new Person2('알바레스', '33');
+console.log(alvarez);
+
+
+
+//ES6, Class
 class Person {
     //클래스 로직
     constructor(name, age) {
