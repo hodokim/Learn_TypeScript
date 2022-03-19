dbd98754089a6cd1c88379fa0a291fbcfdb5cd51
hodokim <hodo323@naver.com>
2022-03-20 00:19:36 +0900

    Comparison of generic and union Type

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -4,8 +4,25 @@
 
 
 function logText<T>(text: T):T {
-    console.log('text');
+    console.log(text);
     return text;
 }
 
-logText<string>('HELLO WORLD');
+const str = logText<string>('HELLO WORLD');
+str.split('');
+
+const flag = logText<boolean>(true);
+
+
+function logThings (text: string | number) {
+    console.log(text);
+    return text;
+}
+
+const unionStr = logThings('a');
+//왜 제네릭인가..???? 아래의 사례를 보자
+
+// 아래의 split api 는 오류가 발생한다.
+//a.split('');
+
+
